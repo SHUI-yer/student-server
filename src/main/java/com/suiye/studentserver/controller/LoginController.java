746caf2229b16127b7d5b878e8a9b764d5eca36c
SHUIYE <Chi_sy@outlook.com>
2026-05-18 22:36:49 +0800

    Initial commit

---
+++ src/main/java/com/suiye/studentserver/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.suiye.studentserver.controller;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class LoginController {
+
+    @PostMapping("/login")
+    public Map<String, Object> login(@RequestBody Map<String, String> user) {
+        String username = user.get("username");
+        String password = user.get("password");
+
+        Map<String, Object> response = new HashMap<>();
+
+        // 这里先用在 DataGrip 里插入的初始管理员数据做比对
+        if ("admin".equals(username) && "123456".equals(password)) {
+            // 登录成功，生成一个 1 天后过期的 JWT Token
+            String token = JWT.create()
+                    .withClaim("username", username)
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
+                    .sign(Algorithm.HMAC256("my_secret_key_123")); // 加密密钥
+
+            response.put("code", 200);
+            response.put("message", "登录成功");
+
+            Map<String, String> data = new HashMap<>();
+            data.put("token", token);
+            response.put("data", data);
+        } else {
+            response.put("code", 400);
+            response.put("message", "用户名或密码错误");
+        }
+
+        return response;
+    }
+}
